929ac1feb386f2be3fecf776ece8b2d18d5ff07f
DanJGV <dangv93@gmail.com>
2020-09-25 00:55:35 -0500

    Initial commit

---
+++ Assets/BasicEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] targets;
+    int targetIndex;
+    Transform currentTarget;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentTarget.position, speed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, currentTarget.position) < dist)
+        {
+            targetIndex++;
+            
+
+            if (targetIndex >= targets.Length )
+            {
+                targetIndex = 0;
+            }
+
+                updateTarget();
+        }
+    }
+
+    void updateTarget()
+    {
+        currentTarget = targets[targetIndex];
+    }
+}
---
+++ Assets/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Text buttonText;
+    public Button myButton;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PressStart()
+    {
+        Debug.Log("Press the Button");
+        buttonText.text = "PRESSED";
+        myButton.image.color = Color.red;
+    }
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
